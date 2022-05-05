b3edba2d5b7097c6f70071acd470524962d863df
Cheng <1003306162@qq.com>
2022-05-05 22:00:21 +0800

    feat: 修改项目根目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 指定入口文件在 src，这时 vite 会主动找 src 下的 index.html
+  root: path.join(__dirname, 'src'),
   plugins: [react()],
 })
