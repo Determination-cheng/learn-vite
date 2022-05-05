a48eeafbd443c1dc0fd846a7409b01b0106c1684
Cheng <1003306162@qq.com>
2022-05-05 22:33:08 +0800

    feat: 完成scss的引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// 全局 scss 文件
+// 使用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(
+  path.resolve('./src/global/css/variable.scss'),
+)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 指定入口文件在 src，这时 vite 会主动找 src 下的 index.html
-  root: path.join(__dirname, 'src'),
+  //* 指定入口文件在 src，这时 vite 会主动找 src 下的 index.html
+  // root: path.join(__dirname, 'src'),
+
+  //* 插件
   plugins: [react()],
+
+  //* css
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 该属性值会被每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
 })
